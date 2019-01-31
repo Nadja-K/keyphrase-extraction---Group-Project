30f7059845ec1fab42e534bc54413983d4a37f70
Nadja Kurz <kasumi5@gmx.de>
2019-01-31 19:06:34 +0100

    Added ranking matrix calculation script

---
+++ src/preprocessing/calculate_ranking_matrix.py
@@ -0,0 +1,119 @@
+import numpy as np
+import glob
+
+from common.DatabaseHandler import DatabaseHandler
+from common.EmbeddingDistributor import EmbeddingDistributor
+from methods.EmbedRank import EmbedRank
+from common.CandidateSelector import CandidateSelector, embed_rank_candidate_selector
+from pathlib import Path
+from sklearn.metrics.pairwise import cosine_distances, cosine_similarity
+import pandas as pd
+
+
+def collect_candidates(document_embeddings, candidate_strings, extractor, filename, sent2vec_model, language,
+                       candidate_selector, regex):
+    # Candidate selection
+    extractor.candidate_selection(candidate_selector=candidate_selector, regex=regex, language=language)
+
+    # Collect the tokenized form of the candidates (which is needed for EmbedRank)
+    for term, candidate in extractor.candidates.items():
+        candidate_strings.add(candidate.tokenized_form)
+
+    # Compute the document embedding as well
+    extractor._compute_document_embedding(sent2vec_model, language)
+    document_embeddings[filename] = extractor.doc_embedding
+
+    return document_embeddings, candidate_strings
+
+
+def compute_candidate_embeddings(sent2vec_model, candidate_strings):
+    candidate_embeddings = sent2vec_model.get_tokenized_sents_embeddings(candidate_strings)
+    print("Candidate embeddings computed")
+
+    # Filter out candidate phrases that were not found in sent2vec
+    valid_candidates_mask = ~np.all(candidate_embeddings == 0, axis=1)
+    candidate_embeddings = candidate_embeddings[valid_candidates_mask, :]
+    # print(candidate_embeddings.shape)
+
+    return candidate_embeddings
+
+
+def compute_candidate_document_similarity(candidate_strings, candidate_embeddings, document_embeddings):
+    # Compute the candidate - document similarity
+    similarity_data = dict()
+    for candidate, candidate_embedding in zip(candidate_strings, candidate_embeddings):
+        if candidate not in similarity_data:
+            similarity_data[candidate] = dict()
+
+        for document, doc_embedding in document_embeddings.items():
+            similarity_data[candidate][document] = cosine_similarity(candidate_embedding.reshape(1, -1), doc_embedding.reshape(1, -1))[0][0]
+
+        # break;
+
+    similarity_dataframe = pd.DataFrame(similarity_data)
+    ranking_dataframe = similarity_dataframe.rank(axis='index', ascending=False)
+
+    return similarity_dataframe, ranking_dataframe
+
+
+def main():
+    # change these depending on the language
+    language = 'en'
+    regex = 'a*n+'
+    sent2vec_model_name = '../word_embedding_models/english/sent2vec/wiki_bigrams.bin'
+
+    # don't change these
+    normalization = 'stemming'
+
+    # data to use (needs to be adapted for the database later) # FIXME
+    dataset = 'DUC'  # SemEval, Inspec, DUC, Heise
+    if dataset == 'SemEval':
+        input_data = "../ake-datasets/datasets/SemEval-2010/train"
+    elif dataset == 'Inspec':
+        input_data = "../ake-datasets/datasets/Inspec/train"
+    elif dataset == 'DUC':
+        input_data = "../ake-datasets/datasets/DUC-2001/test"
+    else:
+        input_data = None
+
+    sent2vec_model = EmbeddingDistributor(sent2vec_model_name)
+    candidate_selector = CandidateSelector(embed_rank_candidate_selector)
+
+    document_embeddings = dict()
+    candidate_strings = set()
+    # Candidate Selection based on the dataset
+    if dataset == 'Heise':
+        db_handler = DatabaseHandler()
+        # FIXME: get train data from db
+        documents, _ = db_handler.load_documents_from_db(EmbedRank, batch_size=200, table='pos_tags',
+                                                                  reference_table='stemmed_filtered_stemmed')
+        for key, doc in documents.items():
+            extractor = doc['document']
+
+            document_embeddings, candidate_strings = collect_candidates(document_embeddings, candidate_strings, extractor, key, sent2vec_model, language, candidate_selector, regex)
+
+    elif input_data is not None:
+        # Collect all candidates and compute the document embeddings
+        for file in glob.glob(input_data + '/*'):
+            extractor = EmbedRank()
+            extractor.load_document(file, language=language, normalization=normalization)
+
+            filename = Path(file).name.split('.')[0]
+            document_embeddings, candidate_strings = collect_candidates(document_embeddings, candidate_strings, extractor, filename, sent2vec_model, language, candidate_selector, regex)
+
+    else:
+        raise Exception("Something went wrong, make sure your input data is declared properly.")
+
+    print("Candidate selection done - Computing embeddings")
+    candidate_embeddings = compute_candidate_embeddings(sent2vec_model, candidate_strings)
+    similarity_dataframe, ranking_dataframe = compute_candidate_document_similarity(candidate_strings, candidate_embeddings, document_embeddings)
+
+    print(similarity_dataframe)
+    print(ranking_dataframe)
+
+    similarity_dataframe.to_pickle(dataset + '_similarity_dataframe')
+    ranking_dataframe.to_pickle(dataset + '_ranking_dataframe')
+
+
+if __name__ == '__main__':
+    main()
