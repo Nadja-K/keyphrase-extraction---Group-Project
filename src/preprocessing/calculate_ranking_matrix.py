Added multiprocessing to the document similarity matrix calculation and fixed a serious bug in that script

src/preprocessing/calculate_ranking_matrix.py
@@ -8,6 +8,9 @@ from common.CandidateSelector import CandidateSelector, embed_rank_candidate_sel
 from pathlib import Path
 from sklearn.metrics.pairwise import cosine_distances, cosine_similarity
 import pandas as pd
+from multiprocessing import Pool
+from itertools import repeat
+import os
 
 
 def collect_candidates(document_embeddings, candidate_strings, extractor, filename, sent2vec_model, language,
@@ -33,24 +36,51 @@ def compute_candidate_embeddings(sent2vec_model, candidate_strings):
     # Filter out candidate phrases that were not found in sent2vec
     valid_candidates_mask = ~np.all(candidate_embeddings == 0, axis=1)
     candidate_embeddings = candidate_embeddings[valid_candidates_mask, :]
-    # print(candidate_embeddings.shape)
 
-    return candidate_embeddings
+    # Filter out candidate phrases from the string list as well
+    filtered_candidate_strings = []
+    candidate_strings = list(candidate_strings)
+    for index, keep in enumerate(valid_candidates_mask):
+        if keep == True:
+            filtered_candidate_strings.append(candidate_strings[index])
+
+    return candidate_embeddings, filtered_candidate_strings
 
 
-def compute_candidate_document_similarity(candidate_strings, candidate_embeddings, document_embeddings):
+def compute_candidate_document_similarity(candidate, candidate_embedding, document_embeddings):
+    similarity_data = dict()
+    similarity_data[candidate] = dict()
+
+    for document, doc_embedding in document_embeddings.items():
+        similarity_data[candidate][document] = \
+        cosine_similarity(candidate_embedding.reshape(1, -1), doc_embedding.reshape(1, -1))[0][0]
+
+    return similarity_data
+
+
+def compute_all_candidate_document_similarity(candidate_strings, candidate_embeddings, document_embeddings):
     # Compute the candidate - document similarity
     similarity_data = dict()
-    for candidate, candidate_embedding in zip(candidate_strings, candidate_embeddings):
-        if candidate not in similarity_data:
-            similarity_data[candidate] = dict()
 
-        for document, doc_embedding in document_embeddings.items():
-            similarity_data[candidate][document] = cosine_similarity(candidate_embedding.reshape(1, -1), doc_embedding.reshape(1, -1))[0][0]
+    input_data = zip(candidate_strings, candidate_embeddings, repeat(document_embeddings))
+    try:
+        pool = Pool(os.cpu_count()-1)
+        results = pool.starmap(compute_candidate_document_similarity, input_data)
+    finally:
+        pool.close()
+        pool.join()
+        [similarity_data.update(res) for res in results]
+
+    # for candidate, candidate_embedding in zip(candidate_strings, candidate_embeddings):
+    #     if candidate not in similarity_data:
+    #         similarity_data[candidate] = dict()
+    #
+    #     for document, doc_embedding in document_embeddings.items():
+    #         similarity_data[candidate][document] = cosine_similarity(candidate_embedding.reshape(1, -1), doc_embedding.reshape(1, -1))[0][0]
 
         # break;
-
     similarity_dataframe = pd.DataFrame(similarity_data)
+    similarity_dataframe = similarity_dataframe.reindex(sorted(similarity_dataframe.columns), axis=1)
     ranking_dataframe = similarity_dataframe.rank(axis='index', ascending=False)
 
     return similarity_dataframe, ranking_dataframe
@@ -66,7 +96,7 @@ def main():
     normalization = 'stemming'
 
     # data to use
-    dataset = 'Heise'  # SemEval-2010, Inspec, DUC-2001, Heise
+    dataset = 'Inspec'  # SemEval-2010, Inspec, DUC-2001, Heise
     if dataset == 'SemEval-2010':
         input_data = "../ake-datasets/datasets/SemEval-2010/train"
     elif dataset == 'Inspec':
@@ -92,7 +122,8 @@ def main():
 
     elif input_data is not None:
         # Collect all candidates and compute the document embeddings
-        for file in glob.glob(input_data + '/*'):
+        index = 0
+        for file in sorted(glob.glob(input_data + '/*')):
             extractor = EmbedRank()
             extractor.load_document(file, language=language, normalization=normalization)
 
@@ -103,11 +134,12 @@ def main():
         raise Exception("Something went wrong, make sure your input data is declared properly.")
 
     print("Candidate selection done - Computing embeddings")
-    candidate_embeddings = compute_candidate_embeddings(sent2vec_model, candidate_strings)
-    similarity_dataframe, ranking_dataframe = compute_candidate_document_similarity(candidate_strings, candidate_embeddings, document_embeddings)
+
+    candidate_embeddings, candidate_strings = compute_candidate_embeddings(sent2vec_model, candidate_strings)
+    similarity_dataframe, ranking_dataframe = compute_all_candidate_document_similarity(candidate_strings, candidate_embeddings, document_embeddings)
 
     print(similarity_dataframe)
-    print(ranking_dataframe)
+    # print(ranking_dataframe)
 
     similarity_dataframe.to_pickle(dataset + '_similarity_dataframe')
     ranking_dataframe.to_pickle(dataset + '_ranking_dataframe')